9a68828f503185b0b44e44d19d9dde904b45bdef
Brian Muhlenkamp <29447475+moskey71@users.noreply.github.com>
2020-01-23 17:34:27 -0500

    test formatting

--- tests/web_connectivity/test-website.py
+++ tests/web_connectivity/test-website.py
@@ -15,37 +15,37 @@ url = args.url
 searchstring = args.searchstring
 
 def page_availability():
-  from urllib.request import Request, urlopen
-  from urllib.error import URLError
-  req = Request(url)
-  try:
-      response = urlopen(req)
-  except URLError as e:
-      if hasattr(e, 'reason'):
-          return f"We failed to reach a server.  Reason: '{e.reason}'"
-      elif hasattr(e, 'code'):
-          return f"The server couldn't fulfill the request.  Error code: '{e.code}'"
-  else:
-      return f"We're good to go"
+    from urllib.request import Request, urlopen
+    from urllib.error import URLError
+    req = Request(url)
+    try:
+        response = urlopen(req)
+    except URLError as e:
+        if hasattr(e, 'reason'):
+            return f"We failed to reach a server.  Reason: '{e.reason}'"
+        elif hasattr(e, 'code'):
+            return f"The server couldn't fulfill the request.  Error code: '{e.code}'"
+    else:
+        return f"We're good to go"
 
 def search_page_content():
-  import urllib.request
-  import re
-  with urllib.request.urlopen(url) as response:
+    import urllib.request
+    import re
+    with urllib.request.urlopen(url) as response:
     html = response.read().decode('utf-8')
     matches = re.findall(searchstring, html)
     if len(matches) == 0:
-      return f"No matches for the phrase '{searchstring}' found!"
+        return f"No matches for the phrase '{searchstring}' found!"
     else:
-      return f"The phrase '{searchstring}' has been found!"
+        return f"The phrase '{searchstring}' has been found!"
 
 def test_page_availability():
-  assert page_availability() == f"We're good to go", f"Page not available"
-  print('page_availability Test Passed')
+    assert page_availability() == f"We're good to go", f"Page not available"
+    print('page_availability Test Passed')
 
 def test_page_content():
-  assert search_page_content() == f"The phrase '{searchstring}' has been found!", f"The phrase '{searchstring}' wasn't found"
-  print('page_content Test Passed')
+    assert search_page_content() == f"The phrase '{searchstring}' has been found!", f"The phrase '{searchstring}' wasn't found"
+    print('page_content Test Passed')
 
 def main():
   test_page_availability()
